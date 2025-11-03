90db662cd75f917528296d2a705fe3d9530004aa
Ngoc Le <ngocle266tqt@gmail.com>
2025-11-03 09:59:21 +0700

    Fix deploy failed on db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
@@ -8,26 +6,31 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({ isGlobal: true }), // load env
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (cfg: ConfigService) => ({
-                type: 'postgres',
-                host: cfg.get<string>('DB_HOST'),
-                port: cfg.get<number>('DB_PORT'),
-                username: cfg.get<string>('DB_USERNAME'),
-                password: cfg.get<string>('DB_PASSWORD'),
-                database: cfg.get<string>('DB_NAME'),
-                entities: [User],
-
-                synchronize: true,
-                logging: false,
-            }),
-        }),
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // load env
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => ({
+        type: 'postgres',
+        host: cfg.get<string>('DB_HOST'),
+        port: cfg.get<number>('DB_PORT'),
+        username: cfg.get<string>('DB_USERNAME'),
+        password: cfg.get<string>('DB_PASSWORD'),
+        database: cfg.get<string>('DB_NAME'),
+        entities: [User],
+        ssl:
+          cfg.get<string>('NODE_ENV') !== 'development'
+            ? {
+              rejectUnauthorized: false,
+            }
+            : undefined,
+        synchronize: true,
+        logging: false,
+      }),
+    }),
     UserModule,
     AuthModule,
-    ],
+  ],
 })
-export class AppModule {}
+export class AppModule { }
